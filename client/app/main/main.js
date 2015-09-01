29a56289ee5532f8a26f70f878f8fc337b5fee0e
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2015-09-01 16:47:39 +0530

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('rstcApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl',
+        authenticate: true
+      });
+  });
